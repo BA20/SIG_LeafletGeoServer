fa7fe7df0b805c18ffd2823d4a7786370594e208
Bernardo Alves <bjca1998@gmail.com>
2021-02-03 23:03:32 +0000

    final

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Confirmar se o Browser suporta a localização
+
 function geoFindMe() {
   if (!navigator.geolocation) {
     console.log("Geolocation is not supported by your browser");
@@ -10,7 +10,6 @@ navigator.geolocation.getCurrentPosition(myLocation);
 function myLocation(position) {
   var details = position.coords;
   console.log(details);
-  //localhost:8080/geoserver/SIG21/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=SIG21%3ABDG_rnap2&maxFeatures=50&outputFormat=application%2Fjson
   function mapwfs() {
     var rootUrl = "http://localhost:8080/geoserver/SIG21/ows?";
 
@@ -38,7 +37,7 @@ function myLocation(position) {
         onEachFeature: onEachFeature,
         pointToLayer: function (feature, latlng) {
           return L.circleMarker(latlng, geojsonMarkerOptions);
-          //return L.marker(latlng);
+       
         },
       }).addTo(map);
     }
@@ -119,7 +118,6 @@ function myLocation(position) {
     layers: [Light],
   });
 
-  ///------------------------------------------------------------------------------------------
 
   var featureGroup = L.featureGroup().addTo(map);
   var drawControl = new L.Control.Draw({
@@ -129,12 +127,22 @@ function myLocation(position) {
   }).addTo(map);
 
   map.on("draw:created", function (e) {
-    // Each time a feaute is created, it's added to the over arching feature group
+   
     featureGroup.addLayer(e.layer);
   });
   map.on("draw:deletestop", function (e) {
     var datajson = featureGroup.toGeoJSON();
     console.log(datajson);
+    axios
+      .post(`http://localhost:3000/saveGeojson`, {
+        json: datajson,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   });
 
   document.getElementById("addLayer").onclick = function (e) {
@@ -143,22 +151,10 @@ function myLocation(position) {
       .then(function (response) {
         console.log("layer");
         var dataJson = JSON.parse(response.data.rows[0].concat);
-        /*  console.log(dataJson);
-        //L.geoJSON(dataJson).addTo(e.layer);
-        var geojsonLayer = L.geoJson(dataJson);
-        featureGroup.addLayer(geojsonLayer);*/
+
         geojsonlayer = L.geoJson(dataJson, {
           onEachFeature: function (feature, layer) {
             featureGroup.addLayer(layer);
-
-            /*   layer.on("click", function (e) {
-              if (selectedFeature) {
-                selectedFeature.editing.disable();
-                // and Here I'll add the code to store my edited polygon in the DB or whatever I want to do with it
-              }
-              selectedFeature = e.target;
-              e.target.editing.enable();
-            });*/
           },
         }).addTo(featureGroup);
       })
@@ -167,12 +163,11 @@ function myLocation(position) {
       });
   };
 
-  // on click, clear all layers
   document.getElementById("delete").onclick = function (e) {
     featureGroup.clearLayers();
   };
   document.getElementById("save").onclick = function (e) {
-    // Extract GeoJson from featureGroup
+   
 
     var datajson = featureGroup.toGeoJSON();
     console.log("asdasdas");
@@ -216,82 +211,76 @@ function myLocation(position) {
     console.log(datageoPl);
     console.log(datageoLs);
     if (!datageoPto.length == 0) {
-      // Stringify the GeoJson
+   
       var convertedData = JSON.stringify(datageoPto);
-      //"text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(datajson));
-
-      //----
+ 
       var ajax = new XMLHttpRequest();
       ajax.open("POST", "http://localhost:3000/savePt", true);
       ajax.setRequestHeader("Content-type", "application/json");
       ajax.send(convertedData);
       ajax.onreadystatechange = function () {
-        // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+       
         if (ajax.readyState == 4 && ajax.status == 200) {
           var data = ajax.responseText;
 
-          // Retorno do Ajax
+  
           console.log(data);
         }
       };
     }
     if (!datageoPl.length == 0) {
-      // Stringify the GeoJson
+      
       var convertedData = JSON.stringify(datageoPl);
-      //"text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(datajson));
-
-      //----
+      
       var ajax = new XMLHttpRequest();
       ajax.open("POST", "http://localhost:3000/savePl", true);
       ajax.setRequestHeader("Content-type", "application/json");
       ajax.send(convertedData);
       ajax.onreadystatechange = function () {
-        // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+        
         if (ajax.readyState == 4 && ajax.status == 200) {
           var data = ajax.responseText;
 
-          // Retorno do Ajax
+          
           console.log(data);
         }
       };
     }
     if (!datageoLs.length == 0) {
-      // Stringify the GeoJson
+      
       var convertedData = JSON.stringify(datageoLs);
-      //"text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(datajson));
-
-      //----
+    
       var ajax = new XMLHttpRequest();
       ajax.open("POST", "http://localhost:3000/saveLs", true);
       ajax.setRequestHeader("Content-type", "application/json");
       ajax.send(convertedData);
       ajax.onreadystatechange = function () {
-        // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
+      
         if (ajax.readyState == 4 && ajax.status == 200) {
           var data = ajax.responseText;
 
-          // Retorno do Ajax
+         
           console.log(data);
         }
       };
     }
     featureGroup.clearLayers();
-    //---
+  
   };
 
-  // document.getElementById("ptos").onclick = function (e) {
+
   function deletePid(id) {
-    /* axios
-        .post(`http://localhost:3000/deletePto`, {
-          id: id,
-        })
-        .then((response) => {
-          console.log(response);
-          loadData();
-        })
-        .catch(function (error) {
-          console.log(error);
-        });*/
+    axios
+      .post(`http://localhost:3000/deletePto`, {
+        id: id,
+      })
+      .then((response) => {
+        console.log(response);
+        loadData();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     console.log(id);
   }
   function deleteLSid(id) {
@@ -328,10 +317,9 @@ function myLocation(position) {
     .catch(function (error) {
       console.log(error);
     });
-  // };
 
-  // document.getElementById("ls").onclick = function (e) {
-  axios
+
+  
     .get("http://localhost:3000/getls")
     .then(function (response) {
       console.log(response.data);
@@ -339,25 +327,17 @@ function myLocation(position) {
     .catch(function (error) {
       console.log(error);
     });
-  // };
-  // document.getElementById("Pl").onclick = function (e) {
-  axios
+ 
     .get("http://localhost:3000/getpl")
     .then(function (response) {
       console.log(response.data);
 
-      // };
+    
     })
     .catch(function (error) {
       console.log(error);
     });
 
-  /*// Create export
-    document
-      .getElementById("export")
-      .setAttribute("href", "data:" + convertedData);
-    document.getElementById("export").setAttribute("download", "data.geojson");
-  };*/
 
   var baseLayers = {
     Light: Light,
